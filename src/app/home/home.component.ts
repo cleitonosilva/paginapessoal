d4069e47e2952359961dd5057ab6dd20f94d0a92
Henrique Mendonca <henrique.mendonca@vh.com.br>
2020-10-01 12:29:36 -0300

    primeiro commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,6 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   showFiller = false;
+  // tiles: Tile[] = [
+  //   { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+  //   { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+  //   { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+  //   { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  // ];
 
   constructor() {}
 
